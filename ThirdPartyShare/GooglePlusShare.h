fdaf104f45c7d69e61b9aecd5956652fe369c376
tbago <tbago@yuneectekiiMac.local>
2015-08-18 11:22:19 +0800

    fixed google plus crash.

--- ThirdPartyShare/GooglePlusShare.h
+++ ThirdPartyShare/GooglePlusShare.h
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2015年 tbago.
+//  Copyright (C) 2015 tbago.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a 
 //  copy of this software and associated documentation files (the "Software"), 
@@ -28,6 +28,6 @@
 
 #import "ThirdPartyShare.h"
 
-@interface GooglePlusShare : NSObject
+@interface GooglePlusShare : ThirdPartyShare
 
 @end
--- ThirdPartyShare/ThirdPartyShare.m
+++ ThirdPartyShare/ThirdPartyShare.m
@@ -1,5 +1,5 @@
 //
-//  Copyright (C) 2015年 tbago.
+//  Copyright (C) 2015 tbago.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a 
 //  copy of this software and associated documentation files (the "Software"), 
